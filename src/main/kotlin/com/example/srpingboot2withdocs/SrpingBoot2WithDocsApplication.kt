eddc2696ad2a4159e093232056459cf8e5e8c745
u_021dt <lnikeshkin@alfabank.ru>
2018-04-05 21:47:29 +0300

    Added project

---
+++ src/main/kotlin/com/example/srpingboot2withdocs/SrpingBoot2WithDocsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.srpingboot2withdocs
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SrpingBoot2WithDocsApplication
+
+fun main(args: Array<String>) {
+    SpringApplication(SrpingBoot2WithDocsApplication::class.java).run(*args)
+}
---
+++ src/test/kotlin/com/example/srpingboot2withdocs/SrpingBoot2WithDocsApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.example.srpingboot2withdocs
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SrpingBoot2WithDocsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
